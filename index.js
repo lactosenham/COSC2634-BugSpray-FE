7565fd50b5b94a4a80d2224afa9dd38ffe4d9947
VNOsST <s3958304@rmit.edu.vn>
2023-11-23 11:33:46 +0700

    finished login page
    Register not done

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-
+const port = 3000;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("layout", "auth-layout");
-
 app.use(express.urlencoded({ extended: true }));
+
 // to apply css styles
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
-// Define routes
-app.get('/', (req, res) => {
-    res.render('test.ejs');
+
+// Routes
+/* Route template 
+
+app.get('<- insert route name here ->', (req, res) => {
+    res.render('layout.ejs/auth-layout.ejs', {
+        title: '<- insert title here ->',
+        bodyFile: '<- insert body file here ->', 
+    });
+
+*/
+app.get('/login', (req, res) => {
+    res.render('authLayout.ejs', {
+        title: 'Home',
+        bodyFile: './auth/login',
+    });
+});
+
+app.get('/register', (req, res) => {
+    res.render('authLayout.ejs', {
+        title: 'Home',
+        bodyFile: './auth/register',
+    });
 });
 
 // Start the server
-const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
