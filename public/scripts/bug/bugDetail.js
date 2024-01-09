5b153e7c4f13f184b85a282590c89363419732f5
To Hoang <122655686+BruhSusBruh@users.noreply.github.com>
2024-01-09 18:19:01 +0700

    bug detail wip

--- public/scripts/bug/bugDetail.js
+++ public/scripts/bug/bugDetail.js
@@ -1,227 +1,144 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bugId = getBugIdFromURL();
   fetchAndDisplayBugDetails(bugId);
-
-  // Event listener to open the popup
-  document
-    .getElementById("assignDeveloperButton")
-    .addEventListener("click", showPopup);
+  setupEventListeners();
 });
 
-ahdb;
 function fetchAndDisplayBugDetails(bugId) {
-  axiosInstance
-    .get(`/api/bugs/${bugId}`)
-    .then(function (response) {
-      const bug = response.data;
-      updateBugUI(bug);
-      fetchAndDisplayDevelopers(bug.projectId);
-    })
-    .catch(function (error) {
-      console.error("Error fetching bug details:", error);
-      // Optionally update the UI to inform the user
-    });
+  axiosInstance.get(`/api/bugs/${bugId}`)
+      .then(function (response) {
+          const bug = response.data;
+          updateBugDetailsUI(bug);
+          fetchAndDisplayDevelopers(bug.projectId);
+      })
+      .catch(function (error) {
+          console.error("Error fetching bug details:", error);
+      });
 }
 
-function updateBugUI(bug) {
-  const bugNameElement = document.querySelector("#name");
-  const saveStatusButton = document.querySelector("#saveStatus");
-  const descriptionElement = document.querySelector("#text");
-  const statusDropdown = document.querySelector("#status");
-  const currentStatus = document.querySelector("#status-level");
-  const reporterName = fetchUserName(bug.reportedBy);
-  const assigneeName = fetchUserName(bug.assignedTo);
-  const dateReportedElement = document.querySelector("#date");
-  const stepsElement = document.querySelector("#steps");
-  const deadlineElement = document.querySelector("#deadline");
-  const priorityElement = document.querySelector("#priority");
-  const severityElement = document.querySelector("#severity");
-
-  bugNameElement.textContent = bug.name;
-  descriptionElement.textContent = bug.description;
-  setStatusDropdown(statusDropdown, bug.status);
-  currentStatus.textContent = bug.status;
-  reporterElement.textContent = bug.reportedBy;
-  assigneeElement.textContent = bug.assignedTo;
-  dateReportedElement.textContent = new Date(
-    bug.reportTime
-  ).toLocaleDateString();
-  stepsElement.textContent = bug.stepsToReproduce;
-  deadlineElement.textContent = new Date(bug.deadline).toLocaleDateString();
-  priorityElement.textContent = bug.priority;
-  severityElement.textContent = bug.severity;
+function updateBugDetailsUI(bug) {
+  document.querySelector("#name").textContent = bug.name;
+  document.querySelector("#text").textContent = bug.description;
+  setStatusDropdown(document.querySelector("#status"), bug.status);
+  document.querySelector("#status-level").textContent = bug.status;
+  document.querySelector("#reporter-name").textContent = bug.reportedBy.name;
+  document.querySelector("#assignee-name").textContent = bug.assignedTo.name;
+  document.querySelector("#date").textContent = new Date(bug.reportTime).toLocaleDateString();
+  document.querySelector("#steps").textContent = bug.stepsToReproduce;
+  document.querySelector("#deadline").textContent = new Date(bug.deadline).toLocaleDateString();
+  document.querySelector("#priority").textContent = bug.priority;
+  document.querySelector("#severity").textContent = bug.severity;
 
   if (bug.resolvedTime) {
-    const resolvedElement = document.querySelector("#resolved");
-    resolvedElement.textContent = new Date(
-      bug.resolvedTime
-    ).toLocaleDateString();
+      document.querySelector("#resolved").textContent = new Date(bug.resolvedTime).toLocaleDateString();
   }
-
-  saveStatusButton.addEventListener("click", function () {
-    const selectedStatus = statusDropdown.value;
-    updateBugStatus(bug._id, selectedStatus);
-  });
 }
 
-function setStatusDropdown(dropdown, currentStatus) {
-  const options = dropdown.options;
-  for (let i = 0; i < options.length; i++) {
-    if (options[i].value === currentStatus) {
-      dropdown.selectedIndex = i;
-      break;
-    }
-  }
+function setupEventListeners() {
+  document.getElementById("assignDeveloperButton").addEventListener("click", showPopup);
+  document.getElementById("closePopup").addEventListener("click", closePopup);
+  document.getElementById("saveStatus").addEventListener("click", saveStatus);
+  document.getElementById('confirmAssign').addEventListener('click', confirmAssignment);
+}
+
+function showPopup() {
+  document.getElementById("developerPopup").classList.remove("hidden");
+}
+
+function closePopup() {
+  document.getElementById("developerPopup").classList.add("hidden");  
+}
+
+function saveStatus() {
+  const selectedStatus = document.querySelector("#status").value;
+  const bugId = getBugIdFromURL();
+  updateBugStatus(bugId, selectedStatus);
 }
 
 async function updateBugStatus(bugId, newStatus) {
-  const updatedData = {
-    status: newStatus,
-  };
-
   try {
-    const response = await axiosInstance.patch(
-      `/api/bugs/update/${bugId}`,
-      updatedData
-    );
-    console.log("Status Updated: ", response.data);
-    // Additional code to handle the response...
+      const response = await axiosInstance.patch(`/api/bugs/update/${bugId}`, { status: newStatus });
+      console.log("Status Updated: ", response.data);
   } catch (error) {
-    console.error("Error updating status: ", error);
-    console.log(error.res.data);
-    // Replace displayErrorMessage with an existing function or use alert/console.log
-    alert("Failed to update status. Please try again."); // Example using alert
+      console.error("Error updating status: ", error);
+      alert("Failed to update status. Please try again.");
   }
 }
 
 function getBugIdFromURL() {
   const currentURL = new URL(window.location.href);
-  const bugId = currentURL.pathname.split("/").pop();
-  return bugId;
+  return currentURL.pathname.split("/").pop();
 }
 
-// Function to show the popup
-function showPopup() {
-  document.getElementById("developerPopup").classList.remove("hidden");
-}
-
-// Function to close the popup
-function closePopup() {
-  document.getElementById("developerPopup").classList.add("hidden");
-}
-
-document.getElementById("closePopup").addEventListener("click", function () {});
-
-let selectedDeveloperId = null; // Variable to store the selected developer's ID
-let selectedDeveloperName = null;
+let selectedDeveloperId = null;
 
 async function fetchAndDisplayDevelopers(projectId) {
   try {
-    const response = await axiosInstance.get(`/api/projects/dev/${projectId}`);
-    const developers = response.data;
-    const developerListElement = document.getElementById("developerList");
-    developerListElement.innerHTML = "";
+      const response = await axiosInstance.get(`/api/projects/dev/${projectId}`);
+      const developers = response.data;
+      const developerListElement = document.getElementById("developerList");
+      developerListElement.innerHTML = "";
 
-    developers.forEach((developer) => {
-      // Create radio button
-      const radioInput = document.createElement("input");
-      radioInput.type = "radio";
-      radioInput.id = developer._id;
-      radioInput.name = "developer";
-      radioInput.value = developer._id;
-      radioInput.classList.add("hidden", "peer");
-
-      // Create label
-      const developerLabel = document.createElement("label");
-      developerLabel.setAttribute("for", developer._id);
-      developerLabel.classList.add(
-        "inline-flex",
-        "items-center",
-        "justify-between",
-        "w-full",
-        "h-full",
-        "p-2",
-        "text-gray-500",
-        "bg-white",
-        "border-2",
-        "border-gray-200",
-        "rounded-lg",
-        "cursor-pointer",
-        "hover:text-gray-600",
-        "peer-checked:border-blue-600",
-        "peer-checked:text-gray-600",
-        "hover:bg-gray-50"
-      );
-
-      // Create content container
-      const contentDiv = document.createElement("div");
-      contentDiv.classList.add("block");
-
-      // Create name element
-      const nameElement = document.createElement("div");
-      nameElement.classList.add("w-full", "text-lg", "font-semibold");
-      nameElement.textContent = developer.name;
-
-      // Add elements to content container and label
-      contentDiv.appendChild(nameElement);
-      developerLabel.appendChild(contentDiv);
-      const labelDiv = document.createElement("div");
-      labelDiv.classList.add("w-full", "flex", "items-center", "h-full");
-      labelDiv.appendChild(radioInput);
-      labelDiv.appendChild(developerLabel);
-
-      // Add event listener for radio selection
-      radioInput.addEventListener("change", () => {
-        selectedDeveloperId = developer._id;
-        selectedDeveloperName = developer.name;
+      developers.forEach((developer) => {
+          const radioInput = createRadioInput(developer);
+          const developerLabel = createDeveloperLabel(developer, radioInput);
+          developerListElement.appendChild(developerLabel);
       });
-
-      developerListElement.appendChild(labelDiv);
-    });
   } catch (error) {
-    console.error("Error fetching developers:", error);
-  }
-
-  document
-    .getElementById("confirmAssign")
-    .addEventListener("click", async () => {
-      if (!selectedDeveloperId) {
-        alert("Please select a developer first.");
-        return;
-      } else {
-        assignDeveloper(selectedDeveloperId, selectedDeveloperName);
-      }
-    });
-
-  async function assignDeveloper(developerId, developerName) {
-    const bugId = getBugIdFromURL(); // Function to get the current bug ID
-    try {
-      const response = await axiosInstance.patch(`/api/bugs/update/${bugId}`, {
-        assignedTo: developerId,
-      });
-      console.log("Developer assigned successfully!", response);
-      // Update the UI to reflect the new assignment
-      fetchAndDisplayDevelopers(bugId);
-      closePopup();
-      updateDevName(developerName);
-    } catch (error) {
-      console.error("Error assigning developer:", error);
-      // Handle error
-    }
+      console.error("Error fetching developers:", error);
   }
 }
 
-async function fetchUserName(userId) {
+function createRadioInput(developer) {
+  const radioInput = document.createElement('input');
+  radioInput.type = 'radio';
+  radioInput.id = developer._id;
+  radioInput.name = 'developer';
+  radioInput.value = developer._id;
+  radioInput.classList.add('hidden', 'peer');
+  radioInput.addEventListener('change', () => selectedDeveloperId = developer._id);
+  return radioInput;
+}
+
+function createDeveloperLabel(developer, radioInput) {
+  const developerLabel = document.createElement('label');
+  developerLabel.setAttribute('for', developer._id);
+  developerLabel.className = 'inline-flex items-center justify-between w-full h-full p-2 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-gray-600 hover:bg-gray-50';
+  const nameElement = document.createElement('div');
+  nameElement.className = 'w-full text-lg font-semibold';
+  nameElement.textContent = developer.name;
+  developerLabel.appendChild(radioInput);
+  developerLabel.appendChild(nameElement);
+  
+  return developerLabel;
+}
+
+function confirmAssignment() {
+  if (!selectedDeveloperId) {
+      alert('Please select a developer first.');
+      return;
+  }
+  assignDeveloper(selectedDeveloperId);
+  closePopup();
+}
+
+async function assignDeveloper(developerId) {
+  const bugId = getBugIdFromURL();
   try {
-    const response = await axiosInstance.get(`/api/users/${userId}`);
-    return response.data.name; // Assuming the user object has a 'name' field
+      const response = await axiosInstance.patch(`/api/bugs/update/${bugId}`, { assignedTo: developerId });
+      console.log('Developer assigned successfully!', response);
   } catch (error) {
-    console.error("Error fetching user details:", error);
-    return "Unknown"; // Fallback name
+      console.error('Error assigning developer:', error);
   }
 }
 
-function updateDevName(devName) {
-  const assigneeElement = document.querySelector("#assignee-name");
-  assigneeElement.textContent = devName;
+function setStatusDropdown(dropdown, currentStatus) {
+  const options = dropdown.options;
+  for (let i = 0; i < options.length; i++) {
+      if (options[i].value === currentStatus) {
+          dropdown.selectedIndex = i;
+          break;
+      }
+  }
 }
+  
